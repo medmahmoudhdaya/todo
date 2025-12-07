d2e4b2c434b3e9109a2450ed0139ea9748762bf7
zidbih <medmahmoudhdaya@gmail.com>
2025-12-07 18:25:19 +0000

    extracted Task component

---
+++ src/Components/Task.tsx
@@ -0,0 +1,8 @@
+export default function Task(){
+    return (
+        <div className="w-full py-1 px-2 shadow rounded-md flex gap-2">
+            <p className="text-xl font-bold text-blue-500 p-1 flex-1">task 1</p>
+            <button className="py-2 px-4 bg-green-500 text-white rounded">edit</button>
+        </div>
+    )
+}
