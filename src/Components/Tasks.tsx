27ea6689efecb69416de74ae25a7cf8c4c442a2b
zidbih <medmahmoudhdaya@gmail.com>
2025-12-07 18:22:37 +0000

    finishing the Tasks component

--- src/Components/Tasks.tsx
+++ src/Components/Tasks.tsx
@@ -1,5 +1,10 @@
 export default function Tasks(){
     return (
-        <p>tasks</p>
+        <div className="w-full p-2 mt-2 flex flex-col gap-2 justify-center items-center">
+            <div className="w-full py-1 px-2 shadow rounded-md flex gap-2">
+                <p className="text-xl font-bold text-blue-500 p-1 flex-1">task 1</p>
+                <button className="py-2 px-4 bg-green-500 text-white rounded">edit</button>
+            </div>
+        </div>
     )
 }
