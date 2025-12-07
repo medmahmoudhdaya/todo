bb86a744a3ef657d8b9e61c8305f97b1f5b7271a
zidbih <medmahmoudhdaya@gmail.com>
2025-12-07 18:15:47 +0000

    finishing the AddTask component

--- src/Components/AddTask.tsx
+++ src/Components/AddTask.tsx
@@ -1,5 +1,8 @@
 export default function AddTask(){
     return (
-        <p>task</p>
+        <div className="w-full p-4 bg-gray-100 flex justify-center items-center gap-2">
+            <input type="text" className="p-2 border outline-none rounded text-blue-500 font-bold text-lg flex-1" />
+            <button className="px-4 py-2 bg-blue-500 text-white rounded">add</button>
+        </div>
     )
 }
