7e5813b540ff3a6c2bc456adbf10c5c2bf7a61ce
zidbih <medmahmoudhdaya@gmail.com>
2025-12-07 20:13:50 +0000

    adding delete functionality

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -2,13 +2,33 @@ import { Trash } from "lucide-react"
 import { SquarePen } from "lucide-react"
 
 export default function Task({ task } : { task : string}){
+
     return (
         <div className="w-full py-1 px-2 shadow rounded-md flex gap-2">
             <p className="text-xl font-bold text-blue-500 p-1 flex-1">{task}</p>
-            <button className="p-2">
+            <button 
+            onClick={() => {
+
+                const tasksLength = Number(localStorage.getItem("tasksLength"))
+
+                for (let i = 0 ; i < tasksLength ; i ++){
+                    const item = localStorage.getItem("name"+(i+1))
+
+                    if (item === task){
+                        localStorage.removeItem("name"+(i+1))
+                        localStorage.setItem("tasksLength", String(tasksLength - 1))
+                    }
+
+                }                
+            }}
+            className="p-2">
                 <Trash color="red"/>
             </button>
-            <button className="p-2">
+            <button 
+            onClick={() => {
+                console.log("edit " + task)
+            }}
+            className="p-2">
                 <SquarePen color="green"/>
             </button>
         </div>
